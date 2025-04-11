fe3da22b01739f9f91ec35d80a333d5d85dbffc9
M Hamzah Ayaz <hamzah.ayaz@stackware.co.uk>
2025-04-11 22:25:27 +0500

    Updated backend libray from gpiozero to rpi.gpio

--- modules/VoltageController.py
+++ modules/VoltageController.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import QObject, Slot, Signal
-from gpiozero import PWMLED
+import RPi.GPIO as GPIO
 import os
 
 class VoltageController(QObject):
@@ -8,27 +8,49 @@ class VoltageController(QObject):
 
     def __init__(self, gpio_pin=19):
         super().__init__()
-        self.led = PWMLED(gpio_pin)
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpio_pin, GPIO.OUT)
+
+        self.pwm = GPIO.PWM(gpio_pin, 5000)  # 5 kHz
+        self.pwm.start(0)  # Start with 0% duty
+
         self.current_voltage = 0.0
 
     @Slot(float)
     def set_voltage(self, voltage):
+
+        if voltage == self.current_voltage:
+            return
+
         try:
+            self.current_voltage = clamped_voltage
             clamped_voltage = max(0.0, min(3.3, voltage))
 
-            if abs(clamped_voltage - self.current_voltage) < 0.001:
-                print(f"No voltage change: {clamped_voltage}V")
-                return
+            pwm_value = round((clamped_voltage / 3.3) * 100 , 1)
+            self.pwm.ChangeDutyCycle(pwm_value)
 
-            self.current_voltage = clamped_voltage
-
-            pwm_value = round(clamped_voltage / 3.3 , 1)
-            self.led.value = pwm_value
-
-            print(f"Voltage set to: {clamped_voltage} V (PWM: {pwm_value})")
-
-            # send signal voltage changed
             self.voltageChanged.emit(clamped_voltage)
+
+            if standalone_run:
+                print(f"Voltage set to: {clamped_voltage} V (PWM: {pwm_value})")
 
         except Exception as e:
             print(f"Error setting voltage: {e}")
+
+    def cleanup(self):
+        self.pwm.stop()
+        GPIO.cleanup(self.gpio_pin)
+
+        print(f"PWM stopped and GPIO{self.gpio_pin} cleaned up")
+
+
+standalone_run = False
+
+if __name__ == "__main__":
+
+    standalone_run = True
+
+    pwmGenerator = VoltageController(19)
+    pwmGenerator.set_voltage(1.5)
+    pwmGenerator.cleanup()
